75d82e6953e92c97f9dd1cd3f3fe86aa11bb34ed
Jannis Nohl <jannis.nohl@uni-ulm.de>
2023-05-11 15:57:24 +0200

    Scanner bleibt an

--- versuch_1/shell.java
+++ versuch_1/shell.java
@@ -1,51 +1,53 @@
 import java.io.File;
-import java.util.Scanner;
-import java.util.List;
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 class Shell {
 
-	public static void main(String [] args){
-		
+	public static void main(String[] args) {
+
+
 		Scanner myObj = new Scanner(System.in); //create scanner object
 		System.out.print("Enter a file name>>>>> ");
-		
-		String userInput = myObj.nextLine(); // read user input
-		String[] inputParts = userInput.split(" ");
-		
-		//create list of input words
-		ArrayList<String> inputList = new ArrayList<String>();
-		
-		
-		//Output user input seperated
-		for(int i = 0; i < inputParts.length; i++){
-			
-			inputList.add(inputParts[i]); //add elements to list
-			
+		while(myObj.hasNext()) {
+			String userInput = myObj.nextLine(); // read user input
+			if(userInput.equals("exit")){
+				myObj.close();
+			}
+			String[] inputParts = userInput.split(" ");
+
+			//create list of input words
+			ArrayList<String> inputList = new ArrayList<String>();
+
+
+			//Output user input seperated
+			for (int i = 0; i < inputParts.length; i++) {
+
+				inputList.add(inputParts[i]); //add elements to list
+
+			}
+
+			// Create an object of the File class
+			// Replace the file path with path of the directory
+
+			File directory = new File(inputList.get(0));
+			findFile(directory);
+
+			System.out.println("inputList: " + inputList);
 		}
-
-		// Create an object of the File class
-		// Replace the file path with path of the directory
-
-		File directory = new File(inputList.get(0));
-		findFile(directory);
-
-		System.out.println("inputList: " + inputList); 
 	}
 
 
-	public static void findFile(File directory){
+	public static void findFile(File directory) {
 		// store all names with same name
 		// with/without extension
 		String[] flist = directory.list();
-		System.out.println(flist	);
+		System.out.println(flist);
 		int flag = 0;
 		if (flist == null) {
 			System.out.println("Empty directory.");
-		}
-		else {
+		} else {
 
 			// Linear search in the array
 			for (int i = 0; i < flist.length; i++) {
@@ -60,6 +62,10 @@ class Shell {
 		if (flag == 0) {
 			System.out.println("File Not Found");
 		}
+
+	}
+
+	public static void write(){
 
 	}
 }
